2966e7aa021f75a3e1da718f6534e90f9cac191f
Coolbiker61 <coolbiker61@gmail.com>
2020-03-08 23:56:10 +0100

    oublie d'un fichier

--- controllers/user.js
+++ controllers/user.js
@@ -32,7 +32,7 @@ exports.login = (req, res, then) => {
                     userId: user._id,
                     token: jwt.sign(
                         { userId: user._id },
-                        'RAMDOM_TOKEN_SECRET',
+                        'c19cf0ee354dee5163bf6b19f0a52cca1892ee15aa5acddd4df33d6a48d62075',
                         { expiresIn: '24h' }
                     )
                 });
