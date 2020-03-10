c09eec1d7d7d399317e74bba3f9b70de749630a8
coolbiker61 <coolbiker61@gmail.com>
2020-03-10 02:36:54 +0100

    soir

--- models/Sauces.js
+++ models/Sauces.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const saucesSchema = mongoose.Schema({
-	userID: { type: String, required: true },
+	userId: { type: String, required: true },
 	name: { type: String, required: true },
 	manufacturer: { type: String, required: true },
 	description: { type: String, required: true },
-    mainingredient: { type: String, required: true },
+    mainPepper: { type: String, required: true },
     imageUrl: { type: String, required: true },
     heat: { type: Number, required: true},
     likes: { type: Number},
